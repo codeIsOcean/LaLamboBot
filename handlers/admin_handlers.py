195565f71e81fe065052a39d99223ac0463a9ee5
QanaSar <qqanatt@gmail.com>
2025-03-14 03:02:48 +0500

    Заменил команду CommandStart на Command('start') так как CommandStart срабатывает на любые отправления боту

--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -1,12 +1,12 @@
 from aiogram import Bot
 from aiogram import Router
 from aiogram.types import Message
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 
 admin_router = Router()
 
 
-@admin_router.message(CommandStart)
+@admin_router.message(Command('start'))
 async def msg_start(message: Message):
     admin_id = message.chat.id
     await message.answer(f'Hello, admin {admin_id}!')
