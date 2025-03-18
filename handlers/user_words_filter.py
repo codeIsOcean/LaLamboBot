13b20a6c7965deff543249d08a37f4a1e58f2db0
QanaSar <qqanatt@gmail.com>
2025-03-18 06:21:55 +0500

    Оптимизировал код для фильтра стоп слов, добавил фильтр ссылок

--- handlers/user_words_filter.py
+++ handlers/user_words_filter.py
@@ -1,49 +1,79 @@
-import re  # импортируется чтобы искать строчку 12h
-from aiogram import Bot, F, Dispatcher
-from aiogram import Router
-from aiogram.types import Message, ChatPermissions
-from aiogram.filters import CommandStart, Command, CommandObject
-from typing import Any
-from aiogram.enums import ParseMode
-from asyncpg.pgproto.pgproto import timedelta
-from pydantic.v1.datetime_parse import parse_time
-from contextlib import suppress  # импортируем чтобы забанить пользователя без ошибок
-from aiogram.exceptions import TelegramBadRequest  # для игнорирования ошибок
-from datetime import datetime, timedelta
-
-from pyexpat.errors import messages
-from pymorphy2 import MorphAnalyzer  # Устанавливается для анализа текста.
+import re
+from aiogram import Bot, Router
+from aiogram.types import Message
+from aiogram.enums.chat_member_status import ChatMemberStatus
+from pymorphy2 import MorphAnalyzer
 
 user_words_filter_router = Router()
-
 morph = MorphAnalyzer()
-ban_words = [
-    r'клоун\w*', r'дура[кч]\w*', r'чмо\w*', r'ло[хш]\w*',
-    r'пид[рa]\w*', r'\w+[е*.]*[eе]+[dд]+'
-]
+
+ban_words = {
+    'клоун', 'дурак', 'чмо', 'лох', 'лош', 'пидр', 'weed', 'сука', 'лошара', 'ешак'
+}
+
+# Символы, которые могут использоваться для обхода фильтра
+replacements = {
+    'а': 'a', 'е': 'e', 'о': 'o', 'р': 'p', 'с': 'c', 'у': 'y', 'х': 'x',
+    '0': 'o', '1': 'l', '3': 'e', '.': '', '*': '', ' ': '', '-': '', '_': '',
+    '!': '', '@': '', '#': '', '$': '', '%': '', '^': '', '&': '', '=': '', '+': ''
+}
+
+# Регулярные выражения для поиска ссылок
+telegram_link_pattern = r"(?:https?://)?(?:t\.me|telegram\.me)/[\w+]+|@\w+"
+url_pattern = r"(?:https?://|www\.)\S+"
 
 
-@user_words_filter_router.message(F.text)
-async def filter_band_words(message: Message, bot: Bot) -> Any:
-    # проверка является ли пользователь админом или простым юзером. Если админ то фильтр на него не действует
+@user_words_filter_router.message()
+async def filter_bad_words(message: Message, bot: Bot):
     chat_member = await bot.get_chat_member(message.chat.id, message.from_user.id)
-    if chat_member.status in ('administrator', 'creator'):
-        return  # Пропускаем проверку для админов
+    if chat_member.status in (ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.CREATOR):
+        return  # Пропускаем админом
 
-    # проверка слов через морфологический анализатор
-    for word in message.text.lower().strip().split():
-        parsed_word = morph.parse(word)[0]
-        normal_form = parsed_word.normal_form
+    text = message.text.lower().strip()  # получаемое смс приводим в нижний регистр и чищем
 
-        for pattern in ban_words:
-            if re.search(pattern, normal_form, re.IGNORECASE):
-                return await message.answer('🤬 не ругайся')
+    # Удаляем пробелы, символы и заменяем буквы
+    normolized_text = text
+    for rus, eng in replacements.items():
+        normolized_text = normolized_text.replace(rus, eng)
 
-        for bad_word in ban_words:
-            if bad_word in normal_form:
-                return await message.answer('🤬 не ругайся')
+    # Проверяем каждое слово, приведенное к нормальной форме
+    words = text.split()
+    normolized_words = {morph.parse(word)[0].normal_form for word in words}
 
-    # дополнительная проверка всего текста
-    for pattern in ban_words:
-        if re.search(pattern, message.text, re.IGNORECASE):
-            return await message.answer('Ну ну ну, не ругайся')
+    # Фильтр запрешенных слов
+    if any(bad_word in normolized_text for bad_word in ban_words) or \
+            any(bad_word in normolized_words for bad_word in ban_words):
+        await message.delete()  # удаляем запрешенно сообшение
+        await message.answer(f'🛑 {message.from_user.first_name}, не ругайся!')
+
+    # Фильтр телеграм-ссылок и любых других ссылок
+    elif re.search(telegram_link_pattern, text) or re.search(url_pattern, text):
+        await message.delete()
+        await message.answer(f'⛔ {message.from_user.first_name}, ссылки запрешенны')
+
+
+# Прежний код
+# @user_words_filter_router.message(F.text)
+# async def filter_band_words(message: Message, bot: Bot) -> Any:
+#     # проверка является ли пользователь админом или простым юзером. Если админ то фильтр на него не действует
+#     chat_member = await bot.get_chat_member(message.chat.id, message.from_user.id)
+#     if chat_member.status in ('administrator', 'creator'):
+#         return  # Пропускаем проверку для админов
+#
+#     # проверка слов через морфологический анализатор
+#     for word in message.text.lower().strip().split():
+#         parsed_word = morph.parse(word)[0]
+#         normal_form = parsed_word.normal_form
+#
+#         for pattern in ban_words:
+#             if re.search(pattern, normal_form, re.IGNORECASE):
+#                 return await message.answer('🤬 не ругайся')
+#
+#         for bad_word in ban_words:
+#             if bad_word in normal_form:
+#                 return await message.answer('🤬 не ругайся')
+#
+#     # дополнительная проверка всего текста
+#     for pattern in ban_words:
+#         if re.search(pattern, message.text, re.IGNORECASE):
+#             return await message.answer('Ну ну ну, не ругайся')
